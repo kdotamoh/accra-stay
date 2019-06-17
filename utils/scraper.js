498e02974eec1ed11e057d968c58272ee344ea58
kwadwo-amoh <21177558+kdotamoh@users.noreply.github.com>
2019-06-17 20:19:44 +0000

    Fetch from meQasa

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 
-const scraper = async () => {
+const scrapeTonaton = async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -10,7 +10,8 @@ const scraper = async () => {
     const scrapedData = await page.evaluate(() => {
       const itemList = [...document.querySelectorAll(".ui-item")];
       const data = itemList.map(item => ({
-        title: item.querySelector("a.item-title").innerText
+        title: item.querySelector("a.item-title").innerText,
+        from: "tonaton"
       }));
       return data;
     });
@@ -20,6 +21,31 @@ const scraper = async () => {
   } catch (err) {
     console.log(err);
   }
+};
+
+const scrapeMeQasa = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://meqasa.com/apartments-for-rent-in-Accra");
+
+  const scrapedData = await page.evaluate(() => {
+    const itemList = [
+      ...document.querySelectorAll(".mqs-featured-prop-inner-wrap")
+    ];
+    const data = itemList.map(item => ({
+      title: item.querySelector(".mqs-prop-dt-wrapper h2 a").innerText,
+      from: "meqasa"
+    }));
+    return data;
+  });
+
+  await browser.close();
+  return scrapedData;
+};
+
+const scraper = async () => {
+  const listings = await Promise.all([scrapeMeQasa(), scrapeTonaton()])
+  return listings;
 };
 
 module.exports = scraper;
